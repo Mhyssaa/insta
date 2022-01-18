c50b8e5576388fee41eb4f1c0d953fb326e5bd69
Aurelien CZN <aurelienczn02@gmail.com>
2022-01-18 14:05:57 +0100

    fix bug

---
+++ assets/profil_perso.php
@@ -0,0 +1,25 @@
+<head>
+    <link rel="stylesheet" href="page_perso.css">
+</head>
+
+<div>
+    <div>
+        <div></div>
+    </div>
+    <p>Pseudo</p>
+    <span></span>
+
+</div>
+
+<div>
+    <p>Créer un post</p>
+</div>
+
+<div>
+    <div class="card">
+        <img src="" alt="">
+    </div>
+    <div>
+
+    </div>
+</div>
