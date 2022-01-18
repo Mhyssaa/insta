c650fe96ac663ff98ebfdbfdbb2f89ebcd86e2fc
Mhyssaa <catherine.wojciechowski@gmail.com>
2022-01-18 14:23:35 +0100

    fichier php créé

---
+++ assets/php/home.php
@@ -0,0 +1,38 @@
+<head>
+
+    <link rel="stylesheet" href="assets/css/home.css">
+
+</head>
+
+<body>
+
+    <main>
+
+        <section id="section1">
+
+            <h1 id="bjr">Bonjour</h1>
+            <div id="barre"></div>
+
+        </section>
+
+        <section id="section2">
+
+            <div>Pseudo</div>
+
+            <div>Image</div>
+
+            <div id="like_com">
+
+                <div><span class="iconify" data-icon="dashicons:heart" style="color: #c276b7;" data-width="50"></span></div>
+
+                <div><span class="iconify" data-icon="bi:chat" style="color: #2b2238;" data-width="50"></span></div>
+                
+            </div>
+
+        </section>
+
+    </main>
+    
+    <script src="https://code.iconify.design/2/2.1.1/iconify.min.js"></script>
+</body>
+</html>
