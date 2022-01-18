c909449dc6adc0812b3a7ed677b8f779efdc0853
Mhyssaa <catherine.wojciechowski@gmail.com>
2022-01-18 11:46:49 +0100

    ajout des fichiers css + js dans les dossiers

---
+++ assets/home.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Evo</title>
+</head>
+<body>
+
+    <main>
+
+        <section>
+
+            <h1>Bonjour</h1>
+            <div class="barre"></div>
+
+        </section>
+
+        <section>
+
+            Pseudo
+            image
+
+            <div>
+                like
+                bulle commentaire
+            </div>
+
+        </section>
+
+        <section>
+
+            <div>
+                <span class="iconify" data-icon="la:fly" style="color: #2b2238;"></span>
+            </div>
+
+        </section>
+
+
+
+    </main>
+    
+
+    <script src="https://code.iconify.design/2/2.1.1/iconify.min.js"></script>
+</body>
+</html>
