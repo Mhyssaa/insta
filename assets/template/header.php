2c105518a7bce283098c20f4a6938c94e4e7201d
cindy bleusez <cindy.bleusez@hotmail.fr>
2022-01-25 15:33:04 +0100

    header en cours

--- assets/template/header.php
+++ assets/template/header.php
@@ -1,23 +1,50 @@
 <head>
-   
+   <link rel="stylesheet" href="assets/css/header.css">
 </head>
 
 <body>
 
-    <header id="header">
+    <header>
 
-    <div>
+    <section id="header">
+
+    <div><img id="logoheader" src="assets/img/Logo.svg" alt="logo"></div>
+
+
+    <!--barre de recherche-->
+
+    <section id="barrederecherche">
+        <div><a href="#">Rechercher</a></div>
+    </section>
+    
+
+    <!--menu burger-->
+
+    <div class="content">
+
+        <div id="btn">
+        <span class="iconify" data-icon="carbon:user-avatar-filled" style="color: #c276b7;" data-width="104" data-height="103"></span>
+        </div>
+
+    </div>
+
+     <!-- liens menu burger <div>
         <a href="index.php?page=connexion">Connexion</a>
         <a href="index.php?page=inscription">Inscription</a>
         <a href="index.php?page=home">Home</a>
         <a href="index.php?page=create_post">Créer post</a>
         <a href="index.php?page=profil_perso">Profil perso</a>
         <a href="index.php?page=mentions">Mentions légales</a>
-    </div>
+    </div>-->
 
-        
+    </section>
+
+    <div id="barre_rose"></div>
 
     </header>
+
+    <script src="assets/js/menuhamburger.js"></script>
+    <script src="https://code.iconify.design/2/2.1.1/iconify.min.js"></script>
 
 </body>
 
